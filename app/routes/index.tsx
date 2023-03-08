09f911748ade9fba63d475d536dcd2c4022fd145
Michael Kranzdorf <Michael.Kranzdorf@xitaso.com>
2023-03-08 10:51:12 +0100

    first try

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <main className="relative min-h-screen bg-white sm:flex flex-col">
       <h1>Hello World 🥳</h1>
+      <h2>I bims 1 Boomer</h2>
     </main>
   );
 }
