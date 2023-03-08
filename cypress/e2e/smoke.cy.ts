9d0cf8793053ea878a6be6c40c5a27a984e981d1
Maximilian Winkelmann <maximilian.winkelmann@xitaso.com>
2023-03-08 10:43:13 +0100

    Fix action and e2e tests

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,7 +1,6 @@
 describe("smoke tests", () => {
   it("should display coop", () => {
     cy.visitAndCheck("/");
-    cy.findByText("Hello World");
-    cy.findByText("Menu");
+    cy.findByText("Hello World 🥳");
   });
 });
