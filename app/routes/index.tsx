ed7660f096e775a1db99bee994ac7b4fb122faf9
Johannes Moll <johannes.moll@xitaso.com>
2023-03-08 10:53:47 +0100

    add to index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <main className="relative min-h-screen bg-white sm:flex flex-col">
       <h1>Hello World 🥳</h1>
+      <p>It works!</p>
     </main>
   );
 }
