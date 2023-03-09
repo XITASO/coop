4833173e5998e319d205b90646ca15db5cb746d5
Johannes Moll <johannes.moll@xitaso.com>
2023-03-09 14:48:05 +0100

    fix test

--- app/routes/menue/index.tsx
+++ app/routes/menue/index.tsx
@@ -7,13 +7,13 @@ export default function Index(){
     return(
         <>
             <Navbar></Navbar>
-            <h1 class="text-2xl">Pizza: </h1>
-            <ul class="ml-3 list-disc">
+            <h1 className="text-2xl">Pizza: </h1>
+            <ul className="ml-3 list-disc">
                 {pizza.map(p => <li>{p}</li>)}
             </ul>
             
-            <hr class="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
-            <h1 class="text-2xl">Pasta: </h1>
+            <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+            <h1 className="text-2xl">Pasta: </h1>
         </>
     );
 }
