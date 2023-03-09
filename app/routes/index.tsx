9c4de945038a6570ee3f35beea27a59151cd815a
michael.damboeck <michael.damboeck@xitaso.com>
2023-03-09 14:53:45 +0100

    PIZ-1 fix e2e tests

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <main className="relative min-h-screen bg-white sm:flex flex-col">
-      <h1>Hello Augsburg 🥳</h1>
+      <h1>Hello World 🥳</h1>
     </main>
   );
 }
